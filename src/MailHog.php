4a52d182ddd82d817088e991b1f36befbb5945b7
roboute <roboute@syneforge.com>
2016-06-16 19:25:32 +0300

    add partial  compatibility with mailhog

--- src/MailHog.php
+++ src/MailHog.php
@@ -12,10 +12,12 @@ namespace Codeception\Module;
 
 use Codeception\Module;
 
+/**
+ * Class MailHog
+ */
 class MailHog extends Module
 {
   use \Codeception\Email\TestsEmails;
-
   use \Codeception\Email\EmailServiceProvider;
 
   /**
@@ -58,110 +60,116 @@ class MailHog extends Module
    *
    * @var array
    */
-  protected $config = array('url', 'port', 'guzzleRequestOptions', 'deleteEmailsAfterScenario');
+  protected $config = ['url', 'port', 'guzzleRequestOptions', 'deleteEmailsAfterScenario'];
 
   /**
    * Codeception required variables
    *
    * @var array
    */
-  protected $requiredFields = array('url', 'port');
+  protected $requiredFields = ['url', 'port'];
 
+
+  /**
+   * Mailhog _initialize
+   *
+   * @return void
+   */
   public function _initialize()
   {
     $url = trim($this->config['url'], '/') . ':' . $this->config['port'];
-
     $this->mailhog = new \GuzzleHttp\Client(['base_uri' => $url, 'timeout' => 1.0]);
 
-    if (isset($this->config['guzzleRequestOptions'])) {
-        foreach ($this->config['guzzleRequestOptions'] as $option => $value) {
-            $this->mailhog->setDefaultOption($option, $value);
-        }
+    if (true === array_key_exists('guzzleRequestOptions', $this->config)) {
+      foreach ($this->config['guzzleRequestOptions'] as $option => $value) {
+        $this->mailhog->setDefaultOption($option, $value);
+      }
     }
   }
 
-  /** 
+  /**
    * Method executed after each scenario
    */
   public function _after(\Codeception\TestCase $test)
   {
-    if(isset($this->config['deleteEmailsAfterScenario']) && $this->config['deleteEmailsAfterScenario'])
-    {
+    if (true === array_key_exists('deleteEmailsAfterScenario', $this->config)
+        && $this->config['deleteEmailsAfterScenario']) {
       $this->deleteAllEmails();
     }
   }
 
-  /** 
+  /**
    * Delete All Emails
    *
-   * Accessible from tests, deletes all emails 
+   * Accessible from tests, deletes all emails
    */
   public function deleteAllEmails()
   {
-    try
-    {
+    try {
       $this->mailhog->request('DELETE', '/api/v1/messages');
-    }
-    catch(Exception $e)
-    {
+    } catch (Exception $e) {
       $this->fail('Exception: ' . $e->getMessage());
     }
+
   }
 
-  /** 
+
+  /**
    * Fetch Emails
    *
-   * Accessible from tests, fetches all emails 
+   * Accessible from tests, fetches all emails
    */
   public function fetchEmails()
   {
     $this->fetchedEmails = array();
 
-    try
-    {
+    try {
       $response = $this->mailhog->request('GET', '/api/v1/messages');
       $this->fetchedEmails = json_decode($response->getBody());
-    }
-    catch(Exception $e)
-    {
+    } catch (Exception $e) {
       $this->fail('Exception: ' . $e->getMessage());
     }
 
     $this->sortEmails($this->fetchedEmails);
 
-    // by default, work on all emails
+    // By default, work on all emails.
     $this->setCurrentInbox($this->fetchedEmails);
   }
 
-  /** 
+  /**
    * Access Inbox For
-   * 
+   *
    * Filters emails to only keep those that are received by the provided address
    *
    * @param string $address Recipient address' inbox
    */
   public function accessInboxFor($address)
   {
-    $inbox = array();
+    $this->fetchEmails();
+    $inbox = [];
     $addressPlusDelimiters = '<' . $address . '>';
-    foreach($this->fetchedEmails as $email)
-    {
-      if(!isset($email->Content->Headers->Bcc))
-      {
-        if(strpos($email->Content->Headers->To[0], $addressPlusDelimiters) || array_search($email->Content->Headers->Cc[0], $addressPlusDelimiters))
-        {
-          array_push($inbox, $email);
+    foreach ($this->fetchedEmails as $email) {
+      if (!isset($email->Content->Headers->Bcc)) {
+        if (!isset($email->Content->Headers->Cc)) {
+          if (false !== strpos($email->Content->Headers->To[0], $address)) {
+            $inbox[] = $email;
+          }
+        } elseif (in_array($address, $email->Content->Headers->Cc[0], true) ||
+            in_array($addressPlusDelimiters, $email->Content->Headers->Cc[0], true)
+        ) {
+          $inbox[] = $email;
         }
-      }
-      else if(strpos($email->Content->Headers->Bcc[0], $addressPlusDelimiters))
-      {
-        array_push($inbox, $email);
+      } elseif (false !== strpos($email->Content->Headers->Bcc[0], $addressPlusDelimiters) ||
+          false !== strpos($email->Content->Headers->Bcc[0], $address)
+      ) {
+        $inbox[] = $email;
       }
     }
     $this->setCurrentInbox($inbox);
   }
 
-  /** 
+
+  /**
    * Open Next Unread Email
    *
    * Pops the most recent unread email and assigns it as the email to conduct tests on
@@ -171,18 +179,19 @@ class MailHog extends Module
     $this->openedEmail = $this->getMostRecentUnreadEmail();
   }
 
+
   /**
    * Get Opened Email
    *
    * Main method called by the tests, providing either the currently open email or the next unread one
    *
-   * @param bool $fetchNextUnread Goes to the next Unread Email
+   * @param boolean $fetchNextUnread Goes to the next Unread Email.
+   *
    * @return mixed Returns a JSON encoded Email
    */
-  protected function getOpenedEmail($fetchNextUnread = FALSE)
+  protected function getOpenedEmail($fetchNextUnread = false)
   {
-    if($fetchNextUnread || $this->openedEmail == NULL)
-    {
+    if ($fetchNextUnread || null === $this->openedEmail) {
       $this->openNextUnreadEmail();
     }
 
@@ -191,15 +200,14 @@ class MailHog extends Module
 
   /**
    * Get Most Recent Unread Email
-   * 
+   *
    * Pops the most recent unread email, fails if the inbox is empty
-   * 
+   *
    * @return mixed Returns a JSON encoded Email
    */
   protected function getMostRecentUnreadEmail()
   {
-    if(empty($this->unreadInbox))
-    {
+    if (0 === count($this->unreadInbox)) {
       $this->fail('Unread Inbox is Empty');
     }
 
@@ -209,24 +217,22 @@ class MailHog extends Module
 
   /**
    * Get Full Email
-   * 
+   *
    * Returns the full content of an email
    *
    * @param string $id ID from the header
+   *
    * @return mixed Returns a JSON encoded Email
    */
   protected function getFullEmail($id)
   {
-    try
-    {
+    try {
       $response = $this->mailhog->request('GET', "/api/v1/messages/{$id}");
-    }
-    catch(Exception $e)
-    {
+    } catch (Exception $e) {
       $this->fail('Exception: ' . $e->getMessage());
     }
-    $fullEmail = json_decode($response->getBody());
-    return $fullEmail;
+
+    return json_decode($response->getBody());
   }
 
   /**
@@ -235,6 +241,7 @@ class MailHog extends Module
    * Returns the subject of an email
    *
    * @param mixed $email Email
+   *
    * @return string Subject
    */
   protected function getEmailSubject($email)
@@ -248,6 +255,7 @@ class MailHog extends Module
    * Returns the body of an email
    *
    * @param mixed $email Email
+   *
    * @return string Body
    */
   protected function getEmailBody($email)
@@ -261,6 +269,7 @@ class MailHog extends Module
    * Returns the string containing the persons included in the To field
    *
    * @param mixed $email Email
+   *
    * @return string To
    */
   protected function getEmailTo($email)
@@ -274,6 +283,7 @@ class MailHog extends Module
    * Returns the string containing the persons included in the CC field
    *
    * @param mixed $email Email
+   *
    * @return string CC
    */
   protected function getEmailCC($email)
@@ -287,15 +297,15 @@ class MailHog extends Module
    * Returns the string containing the persons included in the BCC field
    *
    * @param mixed $email Email
+   *
    * @return string BCC
    */
   protected function getEmailBCC($email)
   {
-    if(isset($email->Content->Headers->Bcc))
-    {
+    if (isset($email->Content->Headers->Bcc)) {
       return $email->Content->Headers->Bcc[0];
     }
-    return "";
+    return '';
   }
 
   /**
@@ -304,15 +314,15 @@ class MailHog extends Module
    * Returns the string containing all of the recipients, such as To, CC and if provided BCC
    *
    * @param mixed $email Email
+   *
    * @return string Recipients
    */
   protected function getEmailRecipients($email)
   {
     $recipients = $email->Content->Headers->To[0] . ' ' .
-                  $email->Content->Headers->Cc[0];
-    if(isset($email->Content->Headers->Bcc))
-    {
-      $recipients .= ' ' . $email->Content->Headers->Bcc[0];  
+        $email->Content->Headers->Cc[0];
+    if (isset($email->Content->Headers->Bcc)) {
+      $recipients .= ' ' . $email->Content->Headers->Bcc[0];
     }
 
     return $recipients;
@@ -324,6 +334,7 @@ class MailHog extends Module
    * Returns the string containing the sender of the email
    *
    * @param mixed $email Email
+   *
    * @return string Sender
    */
   protected function getEmailSender($email)
@@ -337,6 +348,7 @@ class MailHog extends Module
    * Returns the string containing the address to reply to
    *
    * @param mixed $email Email
+   *
    * @return string ReplyTo
    */
   protected function getEmailReplyTo($email)
@@ -346,10 +358,11 @@ class MailHog extends Module
 
   /**
    * Get Email Priority
-   * 
+   *
    * Returns the priority of the email
-   * 
+   *
    * @param mixed $email Email
+   *
    * @return string Priority
    */
   protected function getEmailPriority($email)
@@ -413,12 +426,52 @@ class MailHog extends Module
    *
    * @param mixed $emailA Email
    * @param mixed $emailB Email
+   *
    * @return int Which email should go first
    */
-  static function sortEmailsByCreationDatePredicate($emailA, $emailB) 
+  public static function sortEmailsByCreationDatePredicate($emailA, $emailB)
   {
     $sortKeyA = $emailA->Content->Headers->Date;
     $sortKeyB = $emailB->Content->Headers->Date;
     return ($sortKeyA > $sortKeyB) ? -1 : 1;
+  }
+
+  /**
+   * Integration with Mailcatcher
+   *
+   * Get some data from last email body by pattern
+   *
+   * @param $address
+   * @param $pattern
+   *
+   * @return mixed
+   */
+  public function grabFromLastEmailTo($address, $pattern)
+  {
+    $this->accessInboxFor($address);
+    $lastEmail = $this->currentInbox[count($this->currentInbox) - 1];
+    if (1 !== preg_match($pattern, $lastEmail->Content->Body, $matches)) {
+      $this->assertNotEmpty($matches, "No matches found for $pattern");
+    }
+
+    return preg_replace('/(=)?(\s)/', '', $matches[0]);
+  }
+
+  /**
+   *  Delete all emails
+   */
+  public function resetEmails()
+  {
+    $this->deleteAllEmails();
+  }
+
+  /**
+   * @param int $expected
+   */
+  public function seeEmailCount($expected = 0)
+  {
+    $this->fetchEmails();
+    $count = count($this->fetchedEmails);
+    $this->assertEquals($expected, $count);
   }
 }
